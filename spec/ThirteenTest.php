341b0797b365b3469781a32a43d882011ba0ad94
Stacey Vetzal <stacey@vetzal.com>
2016-05-02 14:45:07 -0400

    clearer method names, some minor cleanup

--- spec/ThirteenTest.php
+++ spec/ThirteenTest.php
@@ -8,35 +8,35 @@ class ThirteenTest extends PHPUnit_Framework_TestCase {
      * @expectedException InvalidArgumentException
      */
     public function test_second_date_should_be_after_first() {
-        Thirteen::countAllInRange(Thirteen::MONDAY, '1973-12-31', '1973-01-01');
+        Thirteen::countDayOfWeekMatchesInRange(Thirteen::MONDAY, '1973-12-31', '1973-01-01');
     }
 
     public function test_Jan_1_1973_Was_A_Monday() {
-        $this->assertEquals(1, Thirteen::countAllInRange(Thirteen::MONDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::TUESDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::WEDNESDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::THURSDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::FRIDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::SATURDAY, '1973-01-01', '1973-01-01'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::SUNDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(1, Thirteen::countDayOfWeekMatchesInRange(Thirteen::MONDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::TUESDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::WEDNESDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::THURSDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::FRIDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::SATURDAY, '1973-01-01', '1973-01-01'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::SUNDAY, '1973-01-01', '1973-01-01'));
     }
 
     public function test_First_Friday_The_Thirteenth_In_1973() {
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::MONDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::TUESDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::WEDNESDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::THURSDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(1, Thirteen::countAllInRange(Thirteen::FRIDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::SATURDAY, '1973-04-13', '1973-04-13'));
-        $this->assertEquals(0, Thirteen::countAllInRange(Thirteen::SUNDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::MONDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::TUESDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::WEDNESDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::THURSDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(1, Thirteen::countDayOfWeekMatchesInRange(Thirteen::FRIDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::SATURDAY, '1973-04-13', '1973-04-13'));
+        $this->assertEquals(0, Thirteen::countDayOfWeekMatchesInRange(Thirteen::SUNDAY, '1973-04-13', '1973-04-13'));
     }
 
     public function test_Fridays_In_1973() {
-        $this->assertEquals(52, Thirteen::countAllInRange(Thirteen::FRIDAY, '1973-01-01', '1973-12-31'));
+        $this->assertEquals(52, Thirteen::countDayOfWeekMatchesInRange(Thirteen::FRIDAY, '1973-01-01', '1973-12-31'));
     }
 
     public function test_Two_Friday_The_13ths_In_1973() {
-        $this->assertEquals(2, Thirteen::countAllWithDayOfMonthInRange(Thirteen::FRIDAY, 13, '1973-01-01', '1973-12-31'));
+        $this->assertEquals(2, Thirteen::countDayOfMonthAndDayOfWeekMatchesInRange(Thirteen::FRIDAY, 13, '1973-01-01', '1973-12-31'));
     }
 
     public function test_Friday_The_13ths_In_40y() {
@@ -57,7 +57,7 @@ class ThirteenTest extends PHPUnit_Framework_TestCase {
     }
 
     private function count13thsOver40years($day) {
-        return Thirteen::countAllWithDayOfMonthInRange($day, 13, '1973-01-01', '2013-12-31');
+        return Thirteen::countDayOfMonthAndDayOfWeekMatchesInRange($day, 13, '1973-01-01', '2013-12-31');
     }
 
 }
--- src/Thirteen.php
+++ src/Thirteen.php
@@ -2,26 +2,26 @@
 
 class Thirteen {
 
+    const SUNDAY    = 0;
     const MONDAY    = 1;
     const TUESDAY   = 2;
     const WEDNESDAY = 3;
     const THURSDAY  = 4;
     const FRIDAY    = 5;
     const SATURDAY  = 6;
-    const SUNDAY    = 0;
 
     private $start;
     private $end;
     private $interval;
 
-    public static function countAllInRange($day, $startDate, $endDate) {
+    public static function countDayOfWeekMatchesInRange($day, $startDate, $endDate) {
         $thirteen = new Thirteen($startDate, $endDate);
-        return $thirteen->countAll($day);
+        return $thirteen->countDayOfWeekMatches($day);
     }
 
-    public static function countAllWithDayOfMonthInRange($weekday, $monthday, $startDate, $endDate) {
+    public static function countDayOfMonthAndDayOfWeekMatchesInRange($weekday, $monthday, $startDate, $endDate) {
         $thirteen = new Thirteen($startDate, $endDate);
-        return $thirteen->countDayOfMonthInRange($weekday, $monthday);
+        return $thirteen->countDayOfMonthAndDayOfWeekMatches($weekday, $monthday);
     }
 
     private function __construct($startDate, $endDate) {
@@ -29,29 +29,29 @@ class Thirteen {
         $this->interval = DateInterval::createFromDateString('1 day');
     }
 
-    private function countAll($day) {
+    private function countDayOfWeekMatches($day) {
         $count = 0;
         foreach($this->rangeInterator() as $date)
-            if ($this->weekdayMatch($day, $date))
+            if ($this->matchesDayOfWeek($day, $date))
                 $count++;
 
         return $count;
     }
 
-    private function countDayOfMonthInRange($weekday, $monthday) {
+    private function countDayOfMonthAndDayOfWeekMatches($weekday, $monthday) {
         $count = 0;
         foreach($this->rangeInterator() as $date)
-            if ($this->weekdayMatch($weekday, $date) && $this->monthdayMatch($monthday, $date))
+            if ($this->matchesDayOfWeek($weekday, $date) && $this->matchesDayOfMonth($monthday, $date))
                 $count++;
 
         return $count;
     }
 
-    private function weekdayMatch($dow, $date) {
+    private function matchesDayOfWeek($dow, $date) {
         return $dow == $date->format('w');
     }
 
-    private function monthdayMatch($monthday, $date) {
+    private function matchesDayOfMonth($monthday, $date) {
         return $monthday == $date->format('j');
     }
 
